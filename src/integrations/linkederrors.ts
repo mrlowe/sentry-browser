ef439f5dbf8aa39cdccb2449d3be1925461490a5
Kamil Ogórek <kamil.ogorek@gmail.com>
2018-09-06 12:09:50 +0200

    feat: LinkedErrors integration for Browser (#1514)

---
+++ src/integrations/linkederrors.ts
@@ -0,0 +1,73 @@
+import { getCurrentHub } from '@sentry/hub';
+import { Integration, SentryEvent, SentryEventHint, SentryException } from '@sentry/types';
+import { exceptionFromStacktrace } from '../parsers';
+import { computeStackTrace } from '../tracekit';
+
+const DEFAULT_KEY = 'cause';
+const DEFAULT_LIMIT = 5;
+
+/**
+ * Just an Error object with arbitrary attributes attached to it.
+ */
+interface ExtendedError extends Error {
+  [key: string]: any;
+}
+
+/** Adds SDK info to an event. */
+export class LinkedErrors implements Integration {
+  /**
+   * @inheritDoc
+   */
+  public readonly name: string = 'LinkedErrors';
+
+  /**
+   * @inheritDoc
+   */
+  private readonly key: string;
+
+  /**
+   * @inheritDoc
+   */
+  private readonly limit: number;
+
+  /**
+   * @inheritDoc
+   */
+  public constructor(options: { key?: string; limit?: number } = {}) {
+    this.key = options.key || DEFAULT_KEY;
+    this.limit = options.limit || DEFAULT_LIMIT;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public install(): void {
+    getCurrentHub().configureScope(scope => {
+      scope.addEventProcessor(this.handler.bind(this));
+    });
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public handler(event: SentryEvent, hint?: SentryEventHint): SentryEvent | null {
+    if (!event.exception || !event.exception.values || !hint || !(hint.originalException instanceof Error)) {
+      return event;
+    }
+    const linkedErrors = this.walkErrorTree(hint.originalException, this.key);
+    event.exception.values = [...event.exception.values, ...linkedErrors];
+    return event;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public walkErrorTree(error: ExtendedError, key: string, stack: SentryException[] = []): SentryException[] {
+    if (!(error[key] instanceof Error) || stack.length >= this.limit) {
+      return stack;
+    }
+    const stacktrace = computeStackTrace(error[key]);
+    const exception = exceptionFromStacktrace(stacktrace);
+    return this.walkErrorTree(error[key], key, [...stack, exception]);
+  }
+}
